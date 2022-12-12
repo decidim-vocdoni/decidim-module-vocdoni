5206c6da615f46e2a1856b146fd8b447715c7a8e
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-12-12 12:37:37 +0100

    Add GraphQL API types and specs
    * Answers
    * Questions
    * Election
    * Elections

---
+++ spec/types/decidim/vocdoni/vocdoni_answer_type_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "decidim/api/test/type_context"
+require "decidim/core/test/shared_examples/attachable_interface_examples"
+require "decidim/core/test/shared_examples/traceable_interface_examples"
+
+module Decidim
+  module Vocdoni
+    describe VocdoniAnswerType, type: :graphql do
+      include_context "with a graphql class type"
+
+      let(:model) { create(:election_answer) }
+
+      it_behaves_like "attachable interface"
+
+      it_behaves_like "traceable interface" do
+        let(:author) { create(:user, :admin, organization: model.component.organization) }
+      end
+
+      describe "id" do
+        let(:query) { "{ id }" }
+
+        it "returns all the required fields" do
+          expect(response).to include("id" => model.id.to_s)
+        end
+      end
+
+      describe "title" do
+        let(:query) { '{ title { translation(locale: "en")}}' }
+
+        it "returns all the required fields" do
+          expect(response["title"]["translation"]).to eq(model.title["en"])
+        end
+      end
+
+      describe "description" do
+        let(:query) { '{ description { translation(locale: "en")}}' }
+
+        it "returns all the required fields" do
+          expect(response["description"]["translation"]).to eq(model.description["en"])
+        end
+      end
+
+      describe "weight" do
+        let(:query) { "{ weight }" }
+
+        it "returns the answers weight" do
+          expect(response["weight"]).to eq(model.weight)
+        end
+      end
+    end
+  end
+end
---
+++ spec/types/decidim/vocdoni/vocdoni_election_type_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "decidim/api/test/type_context"
+require "decidim/core/test/shared_examples/attachable_interface_examples"
+require "decidim/core/test/shared_examples/traceable_interface_examples"
+
+module Decidim
+  module Vocdoni
+    # As there's already a GraphQL type called ElectionType (from the module decidim-elections)
+    # we change the name to VodoniElectionType for the external API.
+    # Note that internally there aren't any colition as Ruby has namespaces, meaning that
+    # Decidim::Vocdoni::Election and Decidim::Vocdoni::Election isn't the same thing.
+    describe VocdoniElectionType, type: :graphql do
+      include_context "with a graphql class type"
+
+      let(:model) { create(:election, :published) }
+
+      it_behaves_like "attachable interface"
+
+      it_behaves_like "traceable interface" do
+        let(:author) { create(:user, :admin, organization: model.component.organization) }
+      end
+
+      describe "id" do
+        let(:query) { "{ id }" }
+
+        it "returns all the required fields" do
+          expect(response).to include("id" => model.id.to_s)
+        end
+      end
+
+      describe "title" do
+        let(:query) { '{ title { translation(locale: "en")}}' }
+
+        it "returns all the required fields" do
+          expect(response["title"]["translation"]).to eq(model.title["en"])
+        end
+      end
+
+      describe "description" do
+        let(:query) { '{ description { translation(locale: "en")}}' }
+
+        it "returns all the required fields" do
+          expect(response["description"]["translation"]).to eq(model.description["en"])
+        end
+      end
+
+      describe "streamUri" do
+        let(:query) { "{ streamUri }" }
+
+        it "returns the election's stream URI" do
+          expect(response["streamUri"]).to eq(model.stream_uri)
+        end
+      end
+
+      describe "startTime" do
+        let(:query) { "{ startTime }" }
+
+        it "returns the election's start time" do
+          expect(Time.zone.parse(response["startTime"])).to be_within(1.second).of(model.start_time)
+        end
+      end
+
+      describe "endTime" do
+        let(:query) { "{ endTime }" }
+
+        it "returns the election's end time" do
+          expect(Time.zone.parse(response["endTime"])).to be_within(1.second).of(model.end_time)
+        end
+      end
+
+      describe "publishedAt" do
+        let(:query) { "{ publishedAt }" }
+
+        it "returns the election's published time" do
+          expect(Time.zone.parse(response["publishedAt"])).to be_within(1.second).of(model.published_at)
+        end
+      end
+
+      describe "blocked" do
+        let(:query) { "{ blocked }" }
+
+        context "when the election's parameters are blocked" do
+          let!(:model) { create(:election, :finished) }
+
+          it "returns true" do
+            expect(response["blocked"]).to be true
+          end
+        end
+
+        context "when the election's parameters are not blocked" do
+          let(:model) { create(:election) }
+
+          it "returns false" do
+            expect(response["blocked"]).to be_falsey
+          end
+        end
+      end
+
+      describe "status" do
+        let(:query) { "{ status }" }
+
+        it "returns the status" do
+          expect(response["status"]).to eq(model.status)
+        end
+      end
+
+      describe "questions" do
+        let!(:election2) { create(:election, :complete) }
+        let(:query) { "{ questions { id } }" }
+
+        it "returns the election questions" do
+          ids = response["questions"].map { |question| question["id"] }
+          expect(ids).to include(*model.questions.map(&:id).map(&:to_s))
+          expect(ids).not_to include(*election2.questions.map(&:id).map(&:to_s))
+        end
+      end
+    end
+  end
+end
