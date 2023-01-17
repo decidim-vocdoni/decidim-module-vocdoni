c3d3ecf867ed3332efbec48ab721fc1c93afc60f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-01-17 12:57:14 +0100

    Don't allow editing an Election if it's blocked

--- app/permissions/decidim/vocdoni/admin/permissions.rb
+++ app/permissions/decidim/vocdoni/admin/permissions.rb
@@ -12,12 +12,14 @@ module Decidim
           when :question, :answer
             case permission_action.action
             when :create, :update, :delete
-              allow!
+              allow_if_not_blocked
             end
           when :election
             case permission_action.action
-            when :create, :read, :update, :publish, :unpublish, :delete
+            when :create, :read
               allow!
+            when :delete, :update, :unpublish, :publish
+              allow_if_not_blocked
             end
           when :steps
             case permission_action.action
@@ -36,8 +38,16 @@ module Decidim
 
         private
 
+        def election
+          @election ||= context.fetch(:election, nil)
+        end
+
         def current_vocdoni_wallet
           @current_vocdoni_wallet ||= Decidim::Vocdoni::Wallet.find_by(decidim_organization_id: user.organization.id)
+        end
+
+        def allow_if_not_blocked
+          toggle_allow(election && !election.blocked?)
         end
       end
     end
