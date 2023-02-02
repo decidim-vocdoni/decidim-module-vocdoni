e695b6a1fb2a714c637b6988b4244f81349d1e99
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-02-02 14:01:48 +0100

    Revert "Add debug message to i18n-tasks unused spec"
    This reverts commit 82efaaa29ac2dd898992c64b5c9f6a9da0818f8b.

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -15,8 +15,7 @@ RSpec.describe I18n do
 
   it 'does not have unused keys' do
     expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them.\n" \
-                           "The unused keys are: #{unused_keys.to_hash}"
+                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
 
   it 'files are normalized' do
