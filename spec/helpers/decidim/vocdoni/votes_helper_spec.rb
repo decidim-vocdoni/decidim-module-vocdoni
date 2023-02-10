e2094a43e58892b40ca2da32f9da0d455c455c73
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-02-10 11:19:40 +0100

    Remove random_answers_order parameter from VotesHelper

--- spec/helpers/decidim/vocdoni/votes_helper_spec.rb
+++ spec/helpers/decidim/vocdoni/votes_helper_spec.rb
@@ -5,41 +5,13 @@ require "spec_helper"
 module Decidim
   module Vocdoni
     describe VotesHelper do
-      let(:question) { create :vocdoni_question, :complete, answers: 3, random_answers_order: random_answers_order }
-      let(:random_answers_order) { true }
+      let(:question) { create :vocdoni_question, :complete, answers: 3 }
 
       let(:helper) do
         Class.new(ActionView::Base) do
           include VotesHelper
           include TranslatableAttributes
         end.new(ActionView::LookupContext.new(ActionController::Base.view_paths), {}, [])
-      end
-
-      describe "ordered_answers" do
-        subject(:uniq_results) { repetitions.times.map { helper.ordered_answers(question) }.uniq }
-
-        let(:repetitions) { 100 }
-
-        it "orders answers in different order on different calls" do
-          # This test could randomly fail with a very low probability (6*(5.0/6)**100, or 1 of 13.802.995 times)
-          expect(uniq_results.length).to eq 6
-        end
-
-        context "when random order is disabled" do
-          let(:repetitions) { 10 }
-          let(:random_answers_order) { false }
-
-          it "orders answers with the same order on every calls" do
-            # This test could randomly result on a false positive with a very low probability ((1.0/6)**9, or 1 of 10.077.696 tiems)
-            expect(uniq_results.length).to eq(1)
-          end
-
-          it "orders answers by weight and creation order" do
-            ordered_ids = question.answers.map { |question| [question.weight, question.id] }.sort.map(&:last)
-
-            expect(uniq_results.first.map(&:id)).to eq(ordered_ids)
-          end
-        end
       end
 
       describe "more_information?" do
