fb2238824b5d9589ca7dd5b548fa8c322c1c2a8a
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-02-24 10:58:31 +0100

    Fix bug when changing elections' steps without status

--- spec/lib/decidim/vocdoni/election_status_changer_spec.rb
+++ spec/lib/decidim/vocdoni/election_status_changer_spec.rb
@@ -7,20 +7,78 @@ describe Decidim::Vocdoni::ElectionStatusChanger do
   subject { described_class.new }
 
   describe "run" do
-    let!(:upcoming_election) { create(:vocdoni_election, :upcoming) }
-    let!(:ongoing_election) { create(:vocdoni_election, :ongoing) }
-    let!(:finished_election) { create(:vocdoni_election, :finished, status: "vote_ended") }
+    let(:new_election) { create(:vocdoni_election) }
+    let(:upcoming_election) { create(:vocdoni_election, :upcoming) }
+    let(:ongoing_election) { create(:vocdoni_election, :ongoing) }
+    let(:finished_election) { create(:vocdoni_election, :finished) }
 
-    # A finished election that hasn't changed the status yet
-    let!(:finished_election2) { create(:vocdoni_election, :finished, status: "vote") }
+    before do
+      # Call all the factories first so they're actually created before
+      # the class is run
+      new_election
+      upcoming_election
+      ongoing_election
+      finished_election
 
-    before { subject.run }
+      subject.run
+    end
 
-    it "works as expected" do
-      expect(upcoming_election.reload.status).to be_nil
-      expect(ongoing_election.reload.status).to eq "vote"
-      expect(finished_election.reload.status).to eq "vote_ended"
-      expect(finished_election2.reload.status).to eq "vote_ended"
+    context "when the elections don't have any status" do
+      let(:new_election) { create(:vocdoni_election) }
+      let(:upcoming_election) { create(:vocdoni_election, :upcoming) }
+      let(:ongoing_election) { create(:vocdoni_election, :ongoing) }
+      let(:finished_election) { create(:vocdoni_election, :finished) }
+
+      it "don't change any status" do
+        expect(new_election.reload.status).to be_nil
+        expect(upcoming_election.reload.status).to be_nil
+        expect(ongoing_election.reload.status).to be_nil
+        expect(finished_election.reload.status).to be_nil
+      end
+    end
+
+    context "with an upcoming election" do
+      let(:upcoming_election) { create(:vocdoni_election, :upcoming, status: "created") }
+
+      it "keeps the same status" do
+        expect(upcoming_election.reload.status).to eq "created"
+      end
+    end
+
+    context "with an ongoing election" do
+      context "with created status" do
+        let(:ongoing_election) { create(:vocdoni_election, :ongoing, status: "created") }
+
+        it "changes the status" do
+          expect(ongoing_election.reload.status).to eq "vote"
+        end
+      end
+
+      context "with vote status" do
+        let(:ongoing_election) { create(:vocdoni_election, :ongoing, status: "vote") }
+
+        it "keeps the same status" do
+          expect(ongoing_election.reload.status).to eq "vote"
+        end
+      end
+    end
+
+    context "with an finished election" do
+      context "with vote status" do
+        let(:finished_election) { create(:vocdoni_election, :finished, status: "vote") }
+
+        it "changes the status" do
+          expect(finished_election.reload.status).to eq "vote_ended"
+        end
+      end
+
+      context "with vote_ended status" do
+        let(:finished_election) { create(:vocdoni_election, :finished, status: "vote_ended") }
+
+        it "keeps the same status" do
+          expect(finished_election.reload.status).to eq "vote_ended"
+        end
+      end
     end
   end
 end
