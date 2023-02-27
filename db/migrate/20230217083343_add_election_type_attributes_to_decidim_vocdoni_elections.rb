66f311ca241e3d150cb57a43bf4186763fd52923
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-02-27 15:16:38 +0100

    Fix rubocop offense

--- db/migrate/20230217083343_add_election_type_attributes_to_decidim_vocdoni_elections.rb
+++ db/migrate/20230217083343_add_election_type_attributes_to_decidim_vocdoni_elections.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddElectionTypeAttributesToDecidimVocdoniElections < ActiveRecord::Migration[6.1]
   def change
     add_column :decidim_vocdoni_elections, :election_type, :jsonb, default: {}
