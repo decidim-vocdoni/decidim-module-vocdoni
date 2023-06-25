8a759f596a1d11f376562663d3cf52c7cd940cc7
Anna Topalidi <t.opalidisanna@gmail.com>
2023-06-25 14:06:49 +0200

    add election_calendar.js

--- app/packs/entrypoints/admin/decidim_vocdoni_admin.js
+++ app/packs/entrypoints/admin/decidim_vocdoni_admin.js
@@ -5,6 +5,7 @@ import "src/decidim/vocdoni/admin/available_credits"
 import "src/decidim/vocdoni/admin/steps/setup_election"
 import "src/decidim/vocdoni/admin/steps/vote"
 import "src/decidim/vocdoni/admin/steps/vote_ended"
+import "src/decidim/vocdoni/admin/election_calendar"
 
 // Images
 require.context("../../images", true)
---
+++ app/packs/src/decidim/vocdoni/admin/election_calendar.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const manualStartCheckbox = document.getElementById("election_calendar_manual_start");
+    const startTimeField = document.getElementById("election_calendar_start_time");
+
+    const toggleStartTimeField = () => {
+        const isManualStartChecked = manualStartCheckbox.checked;
+        startTimeField.disabled = isManualStartChecked;
+        startTimeField.classList.toggle("text-muted", isManualStartChecked);
+    }
+
+    manualStartCheckbox.addEventListener("change", toggleStartTimeField);
+    toggleStartTimeField();
+});
