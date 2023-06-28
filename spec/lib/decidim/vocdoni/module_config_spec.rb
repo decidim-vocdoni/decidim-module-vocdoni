990b56c9408c9df83faffc26563ca103a1b080c4
Anna Topalidi <t.opalidisanna@gmail.com>
2023-06-28 21:56:48 +0200

    fix test

--- spec/lib/decidim/vocdoni/module_config_spec.rb
+++ spec/lib/decidim/vocdoni/module_config_spec.rb
@@ -10,11 +10,13 @@ module Decidim
       let(:env) do
         {
           "VOCDONI_MINUTES_BEFORE_START" => minutes,
-          "VOCDONI_API_ENDPOINT_ENV" => vocdoni_env
+          "VOCDONI_API_ENDPOINT_ENV" => vocdoni_env,
+          "VOCDONI_MANUAL_START_TIME_SETTING_IN_SECONDS" => start_in_seconds
         }
       end
       let(:vocdoni_env) { "STG" }
       let(:minutes) { "11" }
+      let(:start_in_seconds) { "33" }
       let(:config) { JSON.parse cmd_capture("bin/rails runner 'puts Decidim::Vocdoni.config.to_json'", env: env) }
       let(:endpoint_env) { cmd_capture("bin/rails runner 'puts Decidim::Vocdoni.api_endpoint_env'", env: env) }
 
@@ -27,7 +29,8 @@ module Decidim
       it "has the correct configuration" do
         expect(config).to eq({
                                "setup_minimum_minutes_before_start" => 11,
-                               "api_endpoint_env" => "STG"
+                               "api_endpoint_env" => "STG",
+                               "manual_start_time_setting" => 33
                              })
       end
 
