84a83df94edf9eeb23e48a34fe9f8155eed3a81f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-10-09 14:09:23 +0200

    Improve legibility

--- lib/decidim/vocdoni/version.rb
+++ lib/decidim/vocdoni/version.rb
@@ -4,6 +4,7 @@ module Decidim
   # This holds the module version and its compatiblity with Decidim
   module Vocdoni
     COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
+
     def self.version
       "0.27.1"
     end
