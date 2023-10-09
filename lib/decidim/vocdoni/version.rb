c0f6d86a8edf584ec7933469c7787bbe5e73b744
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-10-09 14:09:14 +0200

    Fix comment from generator

--- lib/decidim/vocdoni/version.rb
+++ lib/decidim/vocdoni/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Decidim
-  # This holds the decidim-meetings version.
+  # This holds the module version and its compatiblity with Decidim
   module Vocdoni
     COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
     def self.version
