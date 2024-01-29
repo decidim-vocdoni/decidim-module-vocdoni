2f62529b39758d1cf99597d20f329ec68051bbf3
Ivan Vergés <ivan@pokecode.net>
2024-01-29 15:17:03 +0100

    fix script

--- node-wrapper/test_census.mjs
+++ node-wrapper/test_census.mjs
@@ -4,22 +4,22 @@
  * Manual testing of the Vocdoni SDK Census features
  * */
 process.env.VOCDONI_WRAPPER_PATH = `./`;
-import { PlainCensus, VocdoniSDKClient } from "@vocdoni/sdk";
+import { PlainCensus, VocdoniSDKClient, CensusService, CensusType } from "@vocdoni/sdk";
 import { Wallet } from "@ethersproject/wallet";
 import { vocdoniClient, clientInfo } from "./client.js";
 import { vocdoniElection, updateElectionCensus } from "./election.js";
-import { newElection, runTests } from "./test_helpers.js";
+import { newElection, checkAddress } from "./test_helpers.js";
 
 if (!process.env["WALLET"]) {
   console.error("WALLET env variable not found\n\nTry running:\n\n\tbin/rails runner 'puts Decidim::Vocdoni::Wallet.last.private_key'\n");
   console.log("Usage:\n\n");
-  console.log("\tWALLET=random node node-wrapper/test_census.js\n");
-  console.log("\tWALLET=<private_key> node node-wrapper/test_census.js\n");
-  console.log("\tWALLET=<private_key> ELECTION_DATA={id:<electionId>,censusPrivateKey:<censusPrivateKey>} node node-wrapper/test_census.js\n");
+  console.log("\tENV=dev|stg WALLET=random node node-wrapper/test_census.js\n");
+  console.log("\tENV=dev|stg WALLET=<private_key> node node-wrapper/test_census.js\n");
+  console.log("\tENV=dev|stg WALLET=<private_key> ELECTION_DATA={id:<electionId>,censusPrivateKey:<censusPrivateKey>} node node-wrapper/test_census.js\n");
   process.exit();
 }
 
-const env = "dev";
+const env = process.env["ENV"] || "dev";
 let wallet = null;
 let client = null;
 if(process.env.WALLET == "random") {
@@ -33,32 +33,94 @@ if(process.env.WALLET == "random") {
 	console.log(info);
 	console.log("\n\n");
 	console.log("You can run this script with the following ENV var in order to skip the creation of a new account:");
-	console.log(`WALLET=${wallet.privateKey} node node-wrapper/test_census.mjs`);
+	console.log(`ENV=${env} WALLET=${wallet.privateKey} node node-wrapper/test_census.mjs`);
 	process.env["WALLET"] = wallet.privateKey;
 }
 wallet = new Wallet(process.env.WALLET);
 client = new VocdoniSDKClient({ env, wallet });
+console.log("Using env:", env, "API URL", client.censusService.url)
+
+try {
+	await client.collectFaucetTokens();
+} catch(e) {
+	console.error("Error collecting faucet tokens", e.message);
+}
 
 console.log("Using wallet:");
 console.log({publicKey: wallet.publicKey, privateKey: wallet.privateKey, address: wallet.address});
 
-const census = new PlainCensus()
-census.add(await Wallet.createRandom().getAddress())
-console.log("Using random wallet in the census:");
-console.log(census);
-
-// Create the election
+let census = null;
+let newCensus = null;
+let service = null;
 let electionData = null;
-if(process.env["ELECTION_DATA"]) {
-	electionData = JSON.parse(process.env.ELECTION_DATA);
-	console.log("Using election data from ENV var ELECTION_DATA");
+let censusId = process.env["CENSUS_ID"];
+let censusIdentifier = process.env["CENSUS_IDENTIFIER"];
+const censusWallet = await VocdoniSDKClient.generateWalletFromData("a-test").address;
+const newCensusWallet = await VocdoniSDKClient.generateWalletFromData("a-test-2").address;
+if(process.env["ELECTION_ID"] && process.env["CENSUS_WALLET"] && process.env["CENSUS_ID"] && process.env["CENSUS_IDENTIFIER"]) {
+	console.log("Using census data from ENV vars CENSUS_WALLET, CENSUS_IDENTIFIER and CENSUS_ID");
+  service = new CensusService({ 
+    url: client.censusService.url, 
+    chunk_size: client.censusService.chunk_size,
+    auth: {
+      identifier: process.env["CENSUS_IDENTIFIER"],
+      wallet: new Wallet(process.env["CENSUS_WALLET"])
+    } 
+  });
+
+  console.log("CensusService:",service)
+  census = await service.get(process.env["CENSUS_ID"]);
+	console.log("Census:",census);
+	electionData = await client.fetchElection(process.env.ELECTION_ID);
+	console.log("Using election data from ENV var ELECTION_ID");
+	console.log("ElectionID:", electionData["id"], "Census:", electionData["census"]);
+} else {
+	console.log("Using deterministic wallets in the census:", censusWallet);
+	electionData = await newElection(client, [censusWallet]);
+	console.log("New election created with");
 	console.log(electionData);
+	censusId = electionData["censusId"];
+	censusIdentifier = electionData["censusIdentifier"];
+	service = client.censusService;
+	console.log("\n\n");
+	console.log("You can run this script with the following ENV var in order to skip the creation of a new election:");
+	console.log(`ENV=${env} WALLET=${process.env["WALLET"]} ELECTION_ID=${electionData["id"]} CENSUS_ID=${censusId} CENSUS_IDENTIFIER=${censusIdentifier} CENSUS_WALLET=${electionData["censusPrivateKey"]} node node-wrapper/test_census.mjs`);
 }
 
-electionData = await newElection(client, census);
+console.log("Is the old address in census?")
+console.log(await checkAddress(service, censusId, censusWallet))
+console.log("Is the new addres in census?")
+console.log(await checkAddress(service, censusId, newCensusWallet))
 
-// TODO:
-// 1. let's check the random wallet is in the census
-// 2. Let's change the census
-// 3. Let's check the random wallet is not in the census anymore
-// 4. Let's check the new wallet is in the census
+try {
+	console.log("Adding the new census...");
+	newCensus = await service.create(CensusType.WEIGHTED);
+	console.log(newCensus)
+	const add = await service.add(newCensus.id, [{ key: newCensusWallet, weight: BigInt(1) }]);
+	console.log(add);
+	const publish = await service.publish(newCensus.id);
+	console.log(publish);
+} catch(e) {
+	console.error("Error adding new census", e.message);
+	process.exit();
+}
+
+console.log("Is the old address in census?")
+console.log(await checkAddress(service, newCensus.id, censusWallet))
+console.log("Is the new addres in census?")
+console.log(await checkAddress(service, newCensus.id, newCensusWallet))
+
+try {
+	await client.changeElectionCensus(electionData["id"], newCensus.id, newCensus.uri);
+} catch(e) {
+	console.error("Error updating election census", e);
+}
+
+try {
+	console.log("Is the election using the new census?");
+	const election = await client.fetchElection(electionData["id"]);
+	console.log(election.census);
+} catch(e) {
+	console.error("Error fetching election", e.message);
+	process.exit();
+}
--- node-wrapper/test_helpers.js
+++ node-wrapper/test_helpers.js
@@ -1,15 +1,17 @@
 const { Election, PlainCensus } = require("@vocdoni/sdk");
 const { Wallet } = require("@ethersproject/wallet");
 
-const newElection = async (client, census) => {
-  const election = await Election.from({
+const newElection = async (client, participants) => {
+  const census = new PlainCensus();
+  // Add census
+  participants.forEach((entry) => census.add(entry));
+  const election = Election.from({
     title: 'Election test #' + Math.round(Math.random()*1000000),
     description: 'Election test census',
-    startDate: new Date(Date.now()),
     endDate: new Date(Date.now() + 1000 * 60 * 60 * 24),
     census,
     electionType: {
-      autoStart: true,
+      autoStart: false,
       interruptible: true,
       dynamicCensus: true,
       secretUntilTheEnd: false,
@@ -31,22 +33,22 @@ const newElection = async (client, census) => {
   const id = await client.createElection(election)
   const electionData = {
     "id": id,
+    "censusId": census.censusId,
     "censusIdentifier": client.censusService.auth.identifier,
     "censusAddress": client.censusService.auth.wallet.address,
     "censusPrivateKey": client.censusService.auth.wallet.privateKey,
     "censusPublicKey": client.censusService.auth.wallet.publicKey,
   }
-
-  console.log("New election created with");
-  console.log(electionData);
-  console.log("\n\n");
-  console.log("You can run this script with the following ENV var in order to skip the creation of a new election:");
-  console.log(`WALLET=${process.env["WALLET"]} ELECTION_DATA=${JSON.serialize(electionData)} node node-wrapper/test_census.mjs`);
   return electionData;
 };
 
-const runTests = async () => {
+const checkAddress = async (service, censusId, wallet) => {
+  try {
+    return await service.fetchProof(censusId, wallet) ? "Yes" : "No";
+  } catch(e) {
+    return "No";
+  }
 };
 
 module.exports.newElection = newElection;
-module.exports.runTests = runTests;
+module.exports.checkAddress = checkAddress;
