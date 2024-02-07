445a77a6fd26f482cde91db642bfa4d74c6a543c
Anna Topalidi <t.opalidisanna@gmail.com>
2024-02-07 13:58:43 +0100

    fix locale keys

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,21 +7,7 @@ ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 Decidim::Dev.dummy_app_path = File.expand_path(File.join(__dir__, "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
-require 'selenium/webdriver'
 
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.args << "--explicitly-allowed-ports=#{Capybara.server_port}"
-  options.args << "--headless-new"
-  options.args << "--no-sandbox"
-  options.args << "--window-size=1920,3000"
-  options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: options
-  )
-end
 RSpec.configure do |config|
   config.filter_run_when_matching :focus
   config.profile_examples = 10
