69e56d8a5308dd0d7c701099089880e993e12f52
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2024-02-21 15:43:55 +0100

    add DECIDIM_COMPAT_VERSION
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- lib/decidim/vocdoni/version.rb
+++ lib/decidim/vocdoni/version.rb
@@ -4,6 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module Vocdoni
     DECIDIM_VERSION = "0.27.5"
+    DECIDIM_COMPAT_VERSION = ">= 0.27"
 
     def self.version
       "0.27.5"
