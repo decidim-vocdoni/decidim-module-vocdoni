e65239b5c6082526292bdef5e3b6f990c3d8a624
Anna Topalidi <t.opalidisanna@gmail.com>
2024-02-23 11:43:55 +0100

    add test

--- spec/lib/decidim/vocdoni/module_config_spec.rb
+++ spec/lib/decidim/vocdoni/module_config_spec.rb
@@ -10,6 +10,8 @@ module Decidim
       let(:env) do
         {
           "VOCDONI_MINUTES_BEFORE_START" => minutes,
+          "VOCDONI_RESELLER_NAME" => reseller_name,
+          "VOCDONI_RESELLER_EMAIL" => reseller_email,
           "DECIDIM_VOCDONI_VOTES_OVERWRITE_MAX" => votes_overwrite_max,
           "VOCDONI_API_ENDPOINT_ENV" => vocdoni_env,
           "VOCDONI_MANUAL_START_DELAY" => start_delay
@@ -19,6 +21,8 @@ module Decidim
       let(:minutes) { "11" }
       let(:votes_overwrite_max) { "8" }
       let(:start_delay) { "33" }
+      let(:reseller_name) { "Test reseller" }
+      let(:reseller_email) { "test_reseller@example.org" }
       let(:config) { JSON.parse cmd_capture("bin/rails runner 'puts Decidim::Vocdoni.config.to_json'", env: env) }
       let(:endpoint_env) { cmd_capture("bin/rails runner 'puts Decidim::Vocdoni.api_endpoint_env'", env: env) }
 
@@ -31,6 +35,8 @@ module Decidim
       it "has the correct configuration" do
         expect(config).to eq({
                                "minimum_minutes_before_start" => 11,
+                               "vocdoni_reseller_email" => "test_reseller@example.org",
+                               "vocdoni_reseller_name" => "Test reseller",
                                "votes_overwrite_max" => 8,
                                "api_endpoint_env" => "STG",
                                "manual_start_time_delay" => 33,
