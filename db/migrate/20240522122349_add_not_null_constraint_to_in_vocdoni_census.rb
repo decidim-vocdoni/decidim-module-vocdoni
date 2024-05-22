1f5963b4c2f29b99d1164e1d3740b80e1e364ef6
Anna Topalidi <t.opalidisanna@gmail.com>
2024-05-22 15:52:48 +0200

    add null: false to migration

--- db/migrate/20240522122349_add_not_null_constraint_to_in_vocdoni_census.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddNotNullConstraintToInVocdoniCensus < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :decidim_vocdoni_voters, :in_vocdoni_census, false
-  end
-end
