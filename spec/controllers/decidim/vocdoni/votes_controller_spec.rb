96234594f17fde46cdaa426bee31a632c1df44bb
Ivan Vergés <ivan@pokecode.net>
2024-07-09 13:44:32 +0200

    fix test

--- spec/controllers/decidim/vocdoni/votes_controller_spec.rb
+++ spec/controllers/decidim/vocdoni/votes_controller_spec.rb
@@ -6,7 +6,7 @@ describe Decidim::Vocdoni::VotesController do
   routes { Decidim::Vocdoni::Engine.routes }
 
   let(:component) { create(:vocdoni_component) }
-  let(:election) { create(:vocdoni_election, :published, component:) }
+  let(:election) { create(:vocdoni_election, :published, :started, component:) }
   let(:user) { create(:user, :confirmed, organization: component.organization) }
 
   before do
@@ -35,7 +35,7 @@ describe Decidim::Vocdoni::VotesController do
     it "returns verification status" do
       post :check_verification, params: { election_id: election.id }
       expect(response).to have_http_status(:success)
-      expect(response.body).to eq({ isVerified: false, email: nil, election_id: election.id, token: nil }.to_json)
+      expect(response.body).to eq({ isVerified: false, email: nil, election_id: election.id, token: nil, preview: false }.to_json)
     end
 
     context "when user is verified and is in the Vocdoni census" do
@@ -46,7 +46,7 @@ describe Decidim::Vocdoni::VotesController do
       it "returns verification status" do
         post :check_verification, params: { election_id: election.id }
         expect(response).to have_http_status(:success)
-        expect(response.body).to eq({ isVerified: true, email: voter.email, election_id: election.id, token: voter.token }.to_json)
+        expect(response.body).to eq({ isVerified: true, email: voter.email, election_id: election.id, token: voter.token, preview: false }.to_json)
       end
     end
 
@@ -58,7 +58,7 @@ describe Decidim::Vocdoni::VotesController do
       it "returns verification status" do
         post :check_verification, params: { election_id: election.id }
         expect(response).to have_http_status(:success)
-        expect(response.body).to eq({ isVerified: false, email: voter.email, election_id: election.id, token: voter.token }.to_json)
+        expect(response.body).to eq({ isVerified: false, email: voter.email, election_id: election.id, token: voter.token, preview: false }.to_json)
       end
     end
   end
