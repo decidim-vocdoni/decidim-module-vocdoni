a7567eaecdcad2ed0d6d35b1672bbdebbb33b901
Anna Topalidi <t.opalidisanna@gmail.com>
2024-10-04 13:41:21 +0200

    change polyfill

--- app/packs/src/decidim/vocdoni/global-polyfills.js
+++ app/packs/src/decidim/vocdoni/global-polyfills.js
@@ -1,5 +1,7 @@
+// Adding this polyfill for `process` because `util.js` relies on `process.env.NODE_DEBUG`.
+// This ensures compatibility in environments where `process` is not defined (e.g., browsers).
 window.process = window.process || {
   env: {
-    NODE_ENV: "development"
+    NODE_DEBUG: ""
   }
 };
